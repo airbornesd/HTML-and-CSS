e0a745fc5bef21f83d7fded7eb17bc89ca3c4cbb
airbornesd <dhaka.siddharth1999@gmail.com>
2023-07-07 21:42:32 +0530

    dom intro

---
+++ practiceFolderJS/practice01/chapter7/domWalking.js
@@ -0,0 +1,8 @@
+const navBar = document.getElementById("xyz");
+navBar.style.backgroundColor = "aqua";
+
+document.getElementById("xyzNew").style.color = "red";
+
+
+const collection = document.getElementsByClassName("xyz2")[0];
+collection.style.color = "white";
