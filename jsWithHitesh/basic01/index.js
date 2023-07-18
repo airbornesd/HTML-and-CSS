c422b1d0dd3f40367feb631ad62aea02f60d63f3
airbornesd <dhaka.siddharth1999@gmail.com>
2023-07-18 17:58:50 +0530

    .

---
+++ jsWithHitesh/basic01/index.js
@@ -0,0 +1,21 @@
+// let myState = "uttar pradesh";
+// const myCountry = "india";
+// var myPin = 201011;
+// let myCity;
+
+// console.table([myCountry, myState, myPin, myCity]);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+
+
+// let checkVal = "";
+// let booleanChecked = Boolean(checkVal);
+// console.log(booleanChecked);
+const prompt = require('prompt-sync')();
+const fname = prompt("Enter your first name: ");
+const lname = prompt("Enter your last name: ");
+
+console.log("your name is: ", fname, lname);
+
+console.log(hi);
